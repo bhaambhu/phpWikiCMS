adb614b464ea8d2f8dc16b7650550a6184901d45
Sanjay Bhaambhu <thabhambhu@gmail.com>
2022-12-06 18:56:51 +0530

    Added title bar and styling

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
-?>
-
-</body>
+        include("title-bar.php");
+        ?>
+    </body>
 </html>
---
+++ title-bar.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link rel="shortcut icon" href="./favicon.ico" />
+        <link href="styles/styles.css" type="text/css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Eczar|Ubuntu:400,700" rel="stylesheet">
+        <title>phpWikiCMS</title>
+    </head>
+    <body>
+        <div class="title-section">
+      <img style="margin-left:5; margin-right:-5" src="./favicon.ico" alt="Books Make C and S" width="23" height="23">
+        <a href='./topic.php' style="text-decoration:none;"><h1 class="site-title">PHPWikiCMS</h1></a>
+        <div class="site-subtitle"><i>A php based framework for wikis.</i></div>
+            </div>
+        <hr class="divider" />
